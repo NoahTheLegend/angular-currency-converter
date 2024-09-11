314ab0a494b288b1f2c4c8c8a4065eb026310038
NoahTheLegend <20jeka03@gmail.com>
2024-09-11 18:54:32 +0300

    misc rehaul

--- root/src/app/app.component.ts
+++ root/src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.sass'
 })
 export class AppComponent {
-  title = 'currency-converter-angular';
+  title = 'currency-converter';
 }
