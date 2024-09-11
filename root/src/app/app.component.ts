0d3b480fd34b789b88b21294ed7c5a495e7089b3
NoahTheLegend <20jeka03@gmail.com>
2024-09-12 00:28:49 +0300

    mark up + api researching

--- root/src/app/app.component.ts
+++ root/src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { ApiRequestsService } from "./services/api-requests.service";
+import { AsyncPipe } from '@angular/common';
+import { Observable } from 'rxjs';
+import { inject } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, AsyncPipe],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.sass'
+  styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
-  title = 'currency-converter';
+  currencies$!: Observable<any>;
+  constructor(private api: ApiRequestsService) {}
+
+  ngOnInit(): void {
+    this.currencies$ = this.api.getCurrencies();
+  }
 }
